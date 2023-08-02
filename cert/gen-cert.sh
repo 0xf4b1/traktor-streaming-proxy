82d913255f83908d4ce83bff29ab69f8f8cc8985
0xf4b1 <10889432+0xf4b1@users.noreply.github.com>
2023-08-02 21:46:55 +0200

    Add CN entry to CA certificate

--- cert/gen-cert.sh
+++ cert/gen-cert.sh
@@ -3,7 +3,7 @@
 set -e
 
 openssl genrsa -out ca.key.pem 4096
-openssl req -x509 -new -nodes -key ca.key.pem -sha256 -days 1825 -out ca.pem -subj /
+openssl req -x509 -new -nodes -key ca.key.pem -sha256 -days 1825 -out ca.pem -subj "/CN=Root CA"
 openssl req -new -sha256 -nodes -out server.csr -newkey rsa:4096 -keyout server.key -config <(cat <<EOF
 [req]
 prompt = no
