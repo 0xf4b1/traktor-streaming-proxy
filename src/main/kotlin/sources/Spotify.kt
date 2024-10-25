fa414206413dd96c1f02dd3360c40cb69f7a2305
0xf4b1 <10889432+0xf4b1@users.noreply.github.com>
2024-10-25 03:12:50 +0200

    Move to spotify-kt

--- src/main/kotlin/sources/Spotify.kt
+++ src/main/kotlin/sources/Spotify.kt
@@ -1,21 +1,24 @@
 package sources
 
 import beatport.api.*
-import org.json.JSONArray
-import org.json.JSONObject
+import io.github.tiefensuche.spotify.api.SpotifyApi
 import xyz.gianlu.librespot.audio.decoders.AudioQuality
 import xyz.gianlu.librespot.audio.decoders.VorbisOnlyAudioQuality
 import xyz.gianlu.librespot.core.Session
 import xyz.gianlu.librespot.metadata.TrackId
 import java.io.File
-import java.net.URLEncoder
-import java.util.*
 
 class Spotify : ISource {
 
+    private val _api = SpotifyApi()
+    private val api: SpotifyApi
+        get() {
+            _api.token = token()
+            return _api
+        }
+
     private var session: Session? = null
-    private val nextQueryUrls: HashMap<String, String> = HashMap()
-    private val playlists = mutableListOf<String>()
+    private val playlistIds = mutableListOf<String>()
 
     override val name: String
         get() = "Spotify"
@@ -25,52 +28,45 @@ class Spotify : ISource {
     }
 
     override fun getGenre(): List<Track> {
-        val res = getUsersSavedTracks(true).toMutableList()
+        val res = api.getUsersSavedTracks(true).toMutableList()
         do {
-            val next = getUsersSavedTracks(false)
+            val next = api.getUsersSavedTracks(false)
             res.addAll(next)
         } while (next.isNotEmpty())
-        return res
+        return mapTracks(res)
     }
 
     override fun getCuratedPlaylists(reset: Boolean): List<Playlist> {
-        return getArtists(reset)
+        return mapPlaylists(api.getArtists(reset))
     }
 
     override fun getCuratedPlaylist(id: String): List<Track> {
-        val res = getArtist(id, true).toMutableList()
-        do {
-            val next = getArtist(id, false)
-            res.addAll(next)
-        } while (next.isNotEmpty())
-        return res
+        return getAllTracks(playlistIds[id.toInt()], api::getArtist)
     }
 
     override fun getPlaylists(): List<Playlist> {
-        return getUsersPlaylists(true)
+        return mapPlaylists(api.getUsersPlaylists(true))
     }
 
     override fun getPlaylist(id: String): List<Track> {
-        val res = getPlaylist(id, true).toMutableList()
-        do {
-            val next = getPlaylist(id, false)
-            res.addAll(next)
-        } while (next.isNotEmpty())
-        return res
+        return getAllTracks(playlistIds[id.toInt()], api::getPlaylist)
     }
 
     override fun getTop100(): List<Track> {
-        val res = getReleaseRadar(true).toMutableList()
-        do {
-            val next = getReleaseRadar(false)
-            res.addAll(next)
-        } while (next.isNotEmpty())
-        return res
+        for (category in api.getBrowseCategories(true)) {
+            if (category.name == "New Releases") {
+                for (playlist in api.getCategoryPlaylists(category.id, true)) {
+                    if (playlist.title == "Release Radar") {
+                        return getAllTracks(playlist.id, api::getPlaylist)
+                    }
+                }
+            }
+        }
+        return emptyList()
     }
 
-    override fun query(query: String, refresh: Boolean): List<Track> {
-        val items = request(String.format(QUERY_URL, URLEncoder.encode(query, "utf-8")), refresh, "tracks")
-        return parseTracks(items)
+    override fun query(query: String, reset: Boolean): List<Track> {
+        return mapTracks(api.query(query, reset))
     }
 
     override fun download(id: String): ByteArray {
@@ -92,85 +88,24 @@ class Spotify : ISource {
         } ?: throw Exception("No session!")
     }
 
-    private fun getReleaseRadar(refresh: Boolean): List<Track> {
-        val response = JSONObject(WebRequests.request(WebRequests.createConnection(RELEASE_RADAR_URL, headers = mapOf("Authorization" to token()))).value)
-        val id = response.getJSONObject("playlists").getJSONArray("items").getJSONObject(0).getString("id")
-        playlists.add(id)
-        return getPlaylist((playlists.size - 1).toString(), refresh)
+    private fun getAllTracks(id: String, func: (id: String, refresh: Boolean) -> List<io.github.tiefensuche.spotify.api.Track>): List<Track> {
+        val res = func(id, true).toMutableList()
+        do {
+            val next = func(id, false)
+            res.addAll(next)
+        } while (next.isNotEmpty())
+        return mapTracks(res)
     }
 
-    private fun getUsersSavedTracks(refresh: Boolean): List<Track> {
-        val items = request(USERS_SAVED_TRACKS_URL, refresh)
-        return parseTracks(items)
+    private fun mapTracks(tracks: List<io.github.tiefensuche.spotify.api.Track>): List<Track> {
+        return tracks.map { track -> Track(track.id.substring(track.id.lastIndexOf(':') + 1), listOf(Artist(1, track.artist)), track.title, track.duration) }
     }
 
-    private fun getArtists(refresh: Boolean): List<Playlist> {
-        val items = request(USERS_FOLLOWING, refresh, "artists")
-        return parsePlaylists(items)
-    }
-
-    private fun getArtist(id: String, refresh: Boolean): List<Track> {
-        val items = request(ARTIST_TRACKS.format(playlists[id.toInt()]), refresh, "tracks")
-        return parseTracks(items)
-    }
-
-    private fun getUsersPlaylists(refresh: Boolean): List<Playlist> {
-        val items = request(USERS_PLAYLISTS, refresh)
-        return parsePlaylists(items)
-    }
-
-    private fun getPlaylist(id: String, refresh: Boolean): List<Track> {
-        val items = request(String.format(PLAYLIST_URL, playlists[id.toInt()]), refresh, "tracks")
-        return parseTracks(items)
-    }
-
-    private fun request(url: String, refresh: Boolean, type: String? = null): JSONArray {
-        val request = if (!refresh && url in nextQueryUrls) {
-            if (nextQueryUrls[url] == "null") {
-                return JSONArray()
-            }
-            nextQueryUrls[url].toString()
-        } else {
-            url
+    private fun mapPlaylists(playlists: List<io.github.tiefensuche.spotify.api.Playlist>): List<Playlist> {
+        return playlists.map { artist ->
+            playlistIds.add(artist.id)
+            Playlist((playlistIds.size - 1).toLong(), artist.title)
         }
-        var response = JSONObject(WebRequests.request(WebRequests.createConnection(request, headers = mapOf("Authorization" to token()))).value)
-        if (type != null && response.get(type) is JSONObject) {
-            response = response.getJSONObject(type)
-        }
-        nextQueryUrls[url] = if (!response.isNull("next")) response.getString("next") else "null"
-        if (type != null && response.has(type))
-            return response.getJSONArray(type)
-        return response.getJSONArray("items")
-    }
-
-    private fun parseTracks(items: JSONArray): List<Track> {
-        val result = mutableListOf<Track>()
-        if (items.length() == 0) {
-            return result
-        }
-        for (i in 0 until items.length()) {
-            var item = items.getJSONObject(i)
-            if (item.has("track")) {
-                item = item.getJSONObject("track")
-            }
-
-            val code = item.getString("uri")
-            result.add(Track(code.substring(code.lastIndexOf(':') + 1),
-                listOf(Artist(1, item.getJSONArray("artists").getJSONObject(0).getString("name"))),
-                item.getString("name"), item.getLong("duration_ms"))
-            )
-        }
-        return result
-    }
-
-    private fun parsePlaylists(items: JSONArray): List<Playlist> {
-        val result = mutableListOf<Playlist>()
-        for (i in 0 until items.length()) {
-            val item = items.getJSONObject(i)
-            playlists.add(item.getString("id"))
-            result.add(Playlist(playlists.size.toLong() - 1, item.getString("name")))
-        }
-        return result
     }
 
     private fun streamUri(id: String) {
@@ -185,16 +120,5 @@ class Spotify : ISource {
         proc.outputStream.flush()
         proc.outputStream.close()
         proc.waitFor()
-    }
-
-    companion object {
-        private const val BASE_URL = "https://api.spotify.com/v1"
-        private const val QUERY_URL = "$BASE_URL/search?q=%s&type=track"
-        private const val USERS_SAVED_TRACKS_URL = "$BASE_URL/me/tracks?limit=50"
-        private const val USERS_FOLLOWING = "$BASE_URL/me/following?type=artist&limit=50"
-        private const val ARTIST_TRACKS = "$BASE_URL/artists/%s/top-tracks?market=US"
-        private const val USERS_PLAYLISTS = "$BASE_URL/me/playlists?limit=50"
-        private const val RELEASE_RADAR_URL = "$BASE_URL/search?q=Release-Radar&type=playlist&limit=1"
-        private const val PLAYLIST_URL = "$BASE_URL/playlists/%s"
     }
 }
